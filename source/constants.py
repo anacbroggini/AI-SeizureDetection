6f4fb5b543acf8086a2d24e03eec3d807301a509
Ana Broggini <anacbroggini@gmail.com>
2023-11-09 15:27:23 +0100

    channels added

--- source/constants.py
+++ source/constants.py
@@ -13,7 +13,7 @@
 #     'CZ-PZ'
 #     ]
 
-CHANNELS = ['FP1-F7', 'F7-T7', 'T7-P7', 'P7-O1', 'FP1-F3', 'F3-C3', 'C3-P3', 'P3-O1', 'FZ-CZ', 'CZPZ', 'FP2-F4', 'F4-C4', 'C4-P4', 'P4-O2', 'FP2-F8', 'F8-T8', 'T8-P8'].
+CHANNELS = ['FP1-F7', 'F7-T7', 'T7-P7', 'P7-O1', 'FP1-F3', 'F3-C3', 'C3-P3', 'P3-O1', 'FZ-CZ', 'CZPZ', 'FP2-F4', 'F4-C4', 'C4-P4', 'P4-O2', 'FP2-F8', 'F8-T8', 'T8-P8']
 FREQUENCY_RANGES = {
     'Delta': (0.5, 4),
     'Theta': (4, 8),
